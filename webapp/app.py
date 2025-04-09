0afd73ec87121589044aa7c88fffc664fa39c161
Miguel Martinez <miguel.martinez11@gmail.com>
2025-04-09 12:41:36 +0200

    Start app

---
+++ webapp/app.py
@@ -0,0 +1,36 @@
+from flask import Flask, session
+import os
+import swimclub
+
+app = Flask(__name__)
+app.secret_key = 'YourSecretKey'
+
+@app.get('/')
+def index():
+    return 'Hello, World!'
+
+def populate_data():
+    if "swimmers" not in session:
+        swim_files=os.listdir(swimclub.FOLDER)
+        swim_files.remove('.DS_Store')
+        session["swimmers"]={}
+        for file in swim_files:
+            name,*_=swimclub.read_swim_data(file)
+            if name not in session["swimmers"]:
+                session["swimmers"][name]=[]
+            session["swimmers"][name].append(file)
+
+
+@app.get('/swimmers')
+def display_swimmers():
+    populate_data()
+    return str(sorted(session["swimmers"]))
+
+
+@app.get('/files/<swimmer>')
+def get_swimmers_files(swimmer):
+    return str(session["swimmers"][swimmer])
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
